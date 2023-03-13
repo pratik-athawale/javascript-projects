fbf4c3cc71f523ba6a8efefc4aacaace7e2f5cdb
LeArNinG225 <86876328+LeArNinG225@users.noreply.github.com>
2023-03-13 23:48:45 +0530

    Update script.js

--- 9. piano/script.js
+++ 9. piano/script.js
@@ -2,6 +2,11 @@ bKeysR = document.getElementsByClassName('black-key');
 w = document.querySelector('.white-key').offsetWidth;
 // console.log("width " + w);
 
+audioObjects = [];
+for (let i=1; i<=24; i++){
+    audioObjects[i] = new Audio(`24-piano-keys/key${i}.mp3`);
+}
+
 // applying css to black keys 
 for (let i=0; i<bKeysR.length; i++)
 {
@@ -19,8 +24,7 @@ for (let i=0; i<bKeysR.length; i++)
     bKeysR[i].addEventListener('click', ()=> {
         if (i == 2 || i == 6 || i == 10) return;
         let x = (i < 2)? (i+1): (i < 6)? i: (i < 10)? (i-1): (i-2);
-        m = new Audio(`24-piano-keys/key${x}.mp3`);
-        m.play();
+        audioObjects[x].play();
         // console.log(x + " played");
     })
 }
@@ -30,9 +34,7 @@ wKeysR = document.getElementsByClassName('white-key');
 for (let i=0; i<wKeysR.length; i++)
 {
     wKeysR[i].addEventListener('click', ()=> {
-        m = new Audio(`24-piano-keys/key${i+11}.mp3`);
-        m.play();
-        // console.log(i+11 + " played");
+        audioObjects[i+11].play();
     })
 }
 
